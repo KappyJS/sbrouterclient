f91e11e856500bbfbfe4ae8a8ecfe783ce884463
KappyJS <Kapitanov96@yandex.ru>
2018-12-27 17:15:54 +0300

    Nobody

--- src/comps/cardPage/CardPage.js
+++ src/comps/cardPage/CardPage.js
@@ -18,7 +18,7 @@ import SecondBlock from './secondBlock'
     }
     componentDidMount(){
         console.log(this.props.selected)
-        fetch("https://sbrouterserver.herokuapp.com/"+this.props.match.params.id, {
+        fetch("https://sbrouterserver.herokuapp.com/govs"+this.props.match.params.id, {
             method: "POST",
             headers: {
               Accept: "application/json, text/plain, */*",
